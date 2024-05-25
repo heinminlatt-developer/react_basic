3c6303e306d783030c391921ce89cd6f2d2cfeb0
Hein Min Latt <heinminlatt.ygn001@gmail.com>
2024-05-25 18:50:48 +0630

    User delete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import User from "./components/users/User";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=10")
+      .then((res) => res.json())
+      .then((data) => {
+        const rawUsers = data.results;
+        const filteredUsers = rawUsers.map((usr) => {
+          return {
+            uuid: usr.login.uuid,
+            name: `${usr.name.title} ${usr.name.first} ${usr.name.last}`,
+            phone: usr.phone,
+            cell: usr.cell,
+            photo: usr.picture.thumbnail,
+          };
+        });
+        setUsers(filteredUsers);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const removeUserHandler = (uuid) => {
+    setUsers((prevUsers) => prevUsers.filter((usr) => usr.uuid !== uuid));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <h1 className="text-center my-5 text-info">Our Employees</h1>
+      <div className="row">
+        {users.map((user) => (
+          <User key={user.uuid} data={user} remove={removeUserHandler} />
+        ))}
+      </div>
     </div>
   );
 }
